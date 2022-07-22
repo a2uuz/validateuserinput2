4f2e6d9f001699af4c38a60b3065e7075102e471
Aziz Moe <66800426+a2uuz@users.noreply.github.com>
2022-07-22 19:54:05 +0300

    update

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const passwordInput = document.getElementById("password-input");
 const confirmPassword = document.getElementById("confirm-password");
 const errorMsg = document.getElementById("error-message");
+const submitButton = document.getElementById("submit-button");
 
 confirmPassword.addEventListener("blur", () => {
   if (passwordInput.value === confirmPassword.value) {
